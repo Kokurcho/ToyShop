8f9abc7e7e1f63cf02dd36d476999e87fbd16b64
Kokurcho <kokurindmitry911@gmail.com>
2022-05-26 19:20:54 +0300

    feat: added inline

--- toyshop/admin.py
+++ toyshop/admin.py
@@ -4,5 +4,37 @@ from .models import Toy, Manufacturer
 
 # Register your models here.
 
-admin.site.register(Toy)
-admin.site.register(Manufacturer)
+
+# @admin.register(Toy)
+# class ToyAdmin(admin.ModelAdmin):
+#     list_display = ('name','material','manufacturer')
+
+
+# class ToyInline(admin.TabularInline):
+#     model = Toy
+
+    
+# @admin.register(Manufacturer)
+# class ManufacturerAdmin(admin.ModelAdmin):
+#     list_display = ('name','address')
+#     inlines: [ToyInline]
+
+
+
+#admin.site.register(Toy)
+#admin.site.register(Manufacturer)
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('name','address')
+
+
+class ManufacturerInline(admin.TabularInline):
+    model = Manufacturer
+
+
+@admin.register(Toy)
+class ToyAdmin(admin.ModelAdmin):
+    list_display = ('name','material','manufacturer')
+    inlines: [ManufacturerInline]
